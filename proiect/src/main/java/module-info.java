ded5728422610b2a29297963c0afdf221785ab00
SebastianAmariei30 <sebastian.amariei30@gmail.com>
2025-04-28 18:50:58 +0300

    l4

--- proiect/src/main/java/module-info.java
+++ proiect/src/main/java/module-info.java
@@ -1,9 +1,17 @@
 module tasks {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires log4j;
+
+    opens tasks.model to javafx.base;
     exports tasks.model;
-    exports tasks.services;
+    opens tasks.view to javafx.fxml;
+    exports tasks.view;
+    opens tasks.controller to javafx.fxml;
     exports tasks.controller;
 
-    opens tasks.model to org.junit.platform.commons, org.junit.jupiter.api;
     opens tasks.services to org.junit.platform.commons, org.junit.jupiter.api;
-    opens tasks.controller to org.junit.platform.commons, org.junit.jupiter.api;
+
+
 }
